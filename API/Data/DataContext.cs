3975bfbf4e21f499c307a59fd2486b6185209888
keenan137 <keenan.dup137@gmail.com>
2024-04-26 22:54:45 +0200

    Added DataContext

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+    {
+    }
+
+    // DbSet Properties:
+    public DbSet<Applicant> Applicants { get; set; }
+    public DbSet<Skill> Skills { get; set; }
+}
