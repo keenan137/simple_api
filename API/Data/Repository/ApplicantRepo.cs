9276121dd0593e8bc49c97b58dfc22dc2f42a3a2
keenan137 <keenan.dup137@gmail.com>
2024-04-26 23:55:27 +0200

    Added skeletons of the Repository methods.

--- API/Data/Repository/ApplicantRepo.cs
+++ API/Data/Repository/ApplicantRepo.cs
@@ -1,5 +1,26 @@
-﻿namespace API.Data.Repository;
+﻿using API.Models;
+
+namespace API.Data.Repository;
 
 public class ApplicantRepo : IApplicantRepository
 {
+    public Task<Applicant> GetApplicantById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task AddApplicant(Applicant applicant)
+    {
+        throw new NotImplementedException();
+    }
+    public Task UpdateApplicant(Applicant applicant)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteApplicant(Applicant applicant)
+    {
+        throw new NotImplementedException();
+    }
+
 }
--- API/Data/Repository/IApplicantRepository.cs
+++ API/Data/Repository/IApplicantRepository.cs
@@ -1,5 +1,30 @@
-﻿namespace API.Data.Repository;
+﻿using API.Models;
+
+namespace API.Data.Repository;
 
 public interface IApplicantRepository
 {
+    /// <summary>
+    /// Gets the Applicant by Applicant's Id and includes their Skills.
+    /// </summary>
+    /// <param name="id">Applicant Id</param>
+    Task<Applicant> GetApplicantById(int id);
+
+    /// <summary>
+    /// Adds a new Applicant to the Database, including their skills.
+    /// </summary>
+    /// <param name="applicant">New Applicant</param>
+    Task AddApplicant(Applicant applicant);
+
+    /// <summary>
+    /// Updates the properties of an existing Applicant in the Database, including their skills.
+    /// </summary>
+    /// <param name="applicant"></param>
+    Task UpdateApplicant(Applicant applicant);
+
+    /// <summary>
+    /// Deletes an Applicant from the Database, including their skills.
+    /// </summary>
+    /// <param name="applicant"></param>
+    Task DeleteApplicant(Applicant applicant);
 }
