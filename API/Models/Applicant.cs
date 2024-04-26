eebade4470a2920d377b88959037a5332da30e61
keenan137 <keenan.dup137@gmail.com>
2024-04-26 22:54:29 +0200

    Added Data Model Classes.

---
+++ API/Models/Applicant.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Models;
+
+public class Applicant
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    // Navigation Property
+    public virtual ICollection<Skill> Skills { get; set; }
+}
---
+++ API/Models/Skill.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+public class Skill
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    public int ApplicantId { get; set; }
+
+    // Navigation Property
+    [ForeignKey(nameof(ApplicantId))]
+    public virtual Applicant Applicant { get; set; }
+}
