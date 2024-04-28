c66cbd3a2f4f6205188faaba1e731ca77fdbb6fe
keenan137 <keenan.dup137@gmail.com>
2024-04-29 01:18:03 +0200

    Added to and implemented methods of the Repository.

--- API/Data/Repository/ApplicantRepo.cs
+++ API/Data/Repository/ApplicantRepo.cs
@@ -1,26 +1,183 @@
 ﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data.Repository;
 
 public class ApplicantRepo : IApplicantRepository
 {
-    public Task<Applicant> GetApplicantById(int id)
+    private readonly DataContext _context;
+    private readonly ILogger<ApplicantRepo> _logger;
+
+    public ApplicantRepo(DataContext dataContext, ILogger<ApplicantRepo> logger)
     {
-        throw new NotImplementedException();
+        _context = dataContext;
+        _logger = logger;
     }
 
-    public Task AddApplicant(Applicant applicant)
+    public async Task<List<Applicant>> GetAllApplicants()
     {
-        throw new NotImplementedException();
-    }
-    public Task UpdateApplicant(Applicant applicant)
-    {
-        throw new NotImplementedException();
+        try
+        {
+            return await _context.Applicants
+                .Include(x => x.Skills)
+                .ToListAsync();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An unexpected error occurred.");
+            throw;
+        }
     }
 
-    public Task DeleteApplicant(Applicant applicant)
+    public async Task<Applicant?> GetApplicantById(int id)
     {
-        throw new NotImplementedException();
+        try
+        {
+            return await _context.Applicants
+                .Include(x => x.Skills)
+                .FirstOrDefaultAsync(a => a.Id == id);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An unexpected error occurred.");
+            throw;
+        }
     }
 
+    public async Task<bool> CreateApplicant(Applicant applicant)
+    {
+        try
+        {
+            _context.Applicants.Add(applicant);
+            int addCount = await _context.SaveChangesAsync();
+            return addCount > 0;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An unexpected error occurred.");
+            throw;
+        }
+    }
+
+    public async Task<bool> UpdateApplicant(Applicant applicant)
+    {
+        try
+        {
+            _context.Applicants.Update(applicant);
+            int updateCount = await _context.SaveChangesAsync();
+            return updateCount > 0;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An unexpected error occurred.");
+            throw;
+        }
+    }
+
+    public async Task<bool> DeleteApplicant(int id)
+    {
+        try
+        {
+            Applicant? applicant = await _context.Applicants.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (applicant is null) return false;
+
+            _context.Applicants.Remove(applicant);
+            int deleteCount = await _context.SaveChangesAsync();
+            return deleteCount > 0;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An unexpected error occurred.");
+            throw;
+        }
+    }
+
+    public async Task<List<Skill>> GetSkillsByApplicantId(int id)
+    {
+        try
+        {
+            return await _context.Skills
+                .Where(x => x.ApplicantId == id)
+                .ToListAsync();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An unexpected error occurred.");
+            throw;
+        }
+    }
+
+    public async Task<Skill?> GetSkillById(int id)
+    {
+        try
+        {
+            return await _context.Skills
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An unexpected error occurred.");
+            throw;
+        }
+    }
+
+    public async Task<Skill?> GetSkillByApplicantId(int applicantId, int skillId)
+    {
+        try
+        {
+            return await _context.Skills
+                .FirstOrDefaultAsync(x => x.Id == skillId && x.ApplicantId == applicantId);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An unexpected error occurred.");
+            throw;
+        }
+    }
+
+    public async Task<bool> CreateApplicantSkill(Skill skill)
+    {
+        try
+        {
+            _context.Skills.Add(skill);
+            int addedCount = await _context.SaveChangesAsync();
+            return addedCount > 0;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An unexpected error occurred.");
+            throw;
+        }
+    }
+
+    public async Task<bool> UpdateApplicantSkill(Skill skill)
+    {
+        try
+        {
+            _context.Skills.Update(skill);
+            int updateCount = await _context.SaveChangesAsync();
+            return updateCount > 0;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An unexpected error occurred.");
+            throw;
+        }
+    }
+
+    public async Task<bool> DeleteApplicantSkill(Skill skill)
+    {
+        try
+        {
+            _context.Skills.Remove(skill);
+            int deleteCount = await _context.SaveChangesAsync();
+            return deleteCount > 0;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "An unexpected error occurred.");
+            throw;
+        }
+    }
 }
--- API/Data/Repository/IApplicantRepository.cs
+++ API/Data/Repository/IApplicantRepository.cs
@@ -5,26 +5,80 @@ namespace API.Data.Repository;
 public interface IApplicantRepository
 {
     /// <summary>
+    /// Gets all Applicants in the Database.
+    /// </summary>
+    /// <returns>List of All Applicants.</returns>
+    Task<List<Applicant>> GetAllApplicants();
+
+    /// <summary>
     /// Gets the Applicant by Applicant's Id and includes their Skills.
     /// </summary>
     /// <param name="id">Applicant Id</param>
-    Task<Applicant> GetApplicantById(int id);
+    /// <returns>Applicant | null</returns>
+    Task<Applicant?> GetApplicantById(int id);
 
     /// <summary>
     /// Adds a new Applicant to the Database, including their skills.
     /// </summary>
     /// <param name="applicant">New Applicant</param>
-    Task AddApplicant(Applicant applicant);
+    /// <returns>Create Status</returns>
+    Task<bool> CreateApplicant(Applicant applicant);
 
     /// <summary>
     /// Updates the properties of an existing Applicant in the Database, including their skills.
     /// </summary>
-    /// <param name="applicant"></param>
-    Task UpdateApplicant(Applicant applicant);
+    /// <param name="applicant">Applicant to Update.</param>
+    /// <returns>Update Status</returns>
+    Task<bool> UpdateApplicant(Applicant applicant);
 
     /// <summary>
     /// Deletes an Applicant from the Database, including their skills.
     /// </summary>
-    /// <param name="applicant"></param>
-    Task DeleteApplicant(Applicant applicant);
+    /// <param name="id">Id of the Applicant to delete.</param>
+    /// <returns>Deletion Status</returns>
+    Task<bool> DeleteApplicant(int id);
+
+    /// <summary>
+    /// Gets all the skills of a specified Applicant.
+    /// </summary>
+    /// <param name="id">Id of the Applicant.</param>
+    /// <returns>An applicatn't skills.</returns>
+    Task<List<Skill>> GetSkillsByApplicantId(int id);
+
+    /// <summary>
+    /// Gets a specified skill for a specified Applicant.
+    /// </summary>
+    /// <param name="applicantId">Applicant Id</param>
+    /// <param name="skillId">Skill Id</param>
+    /// <returns>Skill | null</returns>
+    Task<Skill?> GetSkillByApplicantId(int applicantId, int skillId);
+
+    /// <summary>
+    /// Gets the skill by Skill's Id.
+    /// </summary>
+    /// <param name="id">Skill Id</param>
+    /// <returns>Skill | null</returns>
+    Task<Skill?> GetSkillById(int id);
+
+
+    /// <summary>
+    /// Creates/Adds a new skill to an existing applicant.
+    /// </summary>
+    /// <param name="skill">New Skill to be added</param>
+    /// <returns>Whether or now the skill was added.</returns>
+    Task<bool> CreateApplicantSkill(Skill skill);
+
+    /// <summary>
+    /// Updates the properties of an existing Skill in the Database.
+    /// </summary>
+    /// <param name="applicant">Skill to Update.</param>
+    /// <returns>Update Status.</returns>
+    Task<bool> UpdateApplicantSkill(Skill skill);
+
+    /// <summary>
+    /// Deletes the specified skill from the database.
+    /// </summary>
+    /// <param name="skill">Skill to delete.</param>
+    /// <returns>Deletion Status.</returns>
+    Task<bool> DeleteApplicantSkill(Skill skill);
 }
