adb684cdc8e638293bd0df2dd9fd24493d93d586
Keenan Du Plessis <keenan.dup137@gmail.com>
2026-06-11 22:44:33 +0200

    Add API versioning and explorer support
    Added Asp.Versioning to configure default API version, enable version reporting, and support version reading from URL segments and headers. Configured API explorer for versioned documentation with Swagger.

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,28 @@
 using API.Data;
 using API.Data.Repository;
+using Asp.Versioning;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.DefaultApiVersion = new ApiVersion(1);
+    options.ReportApiVersions = true;                       // Include the "API-supported-versions" and "API-deprecated-versions" headers in responses.
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new UrlSegmentApiVersionReader(),
+        new HeaderApiVersionReader("X-Api-Version"));       // Read the API version from the URL segment and the "X-Api-Version" header.
+})
+.AddMvc()
+.AddApiExplorer(options =>                                  // Add API explorer to discover versions and generate documentation - Swagger.
+{
+    options.GroupNameFormat = "'v'V";                       // Format the group name as "v1", "v2", etc.
+    options.SubstituteApiVersionInUrl = true;               // Substitute the API version in the URL when generating documentation.
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
